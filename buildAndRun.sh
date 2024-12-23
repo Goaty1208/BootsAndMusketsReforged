6977c67dd3c1554dbb817251adca4fec41ab6d18
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2024-12-23 12:40:11 +0100

    Fixed Camera

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,7 +1,7 @@
 cmake -S . -B build
 cmake --build build
-cp -a ./src/build/. ./build/bin/
-cd ./build/bin/
-./bootsAndMuskets
+cp -a ./src/build/. ./build/bin/debug/
+cd ./build/bin/debug/
+gdb bootsAndMuskets
 cd ..
 cd ..
